82c5dc47cc8ec6074890a866f0d70cb27f4e13b1
Dewflio <43574262+Dewflio@users.noreply.github.com>
2024-04-17 16:57:46 +0200

    some changes I guess

--- leo.R
+++ leo.R
@@ -2,11 +2,7 @@ library(magrittr)
 library(tidyverse)
 library(dplyr) # mutate
 library(caret) # confusion matrix
-library(nortest) # Anderson-Darling test for normality
-library(ROCit) # rocit() - optimal cut-off
-library(pROC) # roc() - create ROC curve
-library(MASS)
-
+library(randomForest)
 
 data <- read_csv("spotify_top_songs_audio_features.csv", col_names = TRUE, num_threads = 4)
 head(data)
@@ -21,7 +17,6 @@ heatmap(numeric_correlation_matrix,
         col = colorRampPalette(c("blue", "white", "red"))(100),
         symm = TRUE)
 
-
 # Regression models
 
 # Model 1:  acousticness ~ danceability + loudness + energy
@@ -29,3 +24,42 @@ hist(data$acousticness, col="green")
 
 # Model 2:  energy ~ loudness + valence
 hist(data$energy, col="green")
+
+
+model1_data <- data[, c("acousticness", "loudness", "energy", "danceability")]
+
+set.seed(1)
+train_indices <- createDataPartition(model1_data$acousticness, p = 0.8, list = FALSE)
+train_data <- model1_data[train_indices, ]
+test_data <- model1_data[-train_indices, ]
+
+model <- randomForest(acousticness ~ ., data = train_data)
+
+predictions <- predict(model, newdata = test_data)
+mse <- mean((test_data$acousticness - predictions)^2)
+mse
+
+varImpPlot(model)
+
+residuals <- test_data$acousticness - predictions
+plot(predictions, residuals, xlab = "Predicted", ylab = "Residuals", main = "Residual Plot")
+
+
+#Improving the random forest
+
+tuneGrid <- expand.grid(
+  ntree = c(50, 100, 200),
+  mtry = c(2, 3)
+)
+ctrl <- trainControl(method = "cv", number = 5)
+ctrl
+
+tuned_model <- train(
+  acousticness ~ .,
+  data = train_data,
+  method = "rf",
+  tuneGrid = tuneGrid,
+  trControl = ctrl
+)
+
+
